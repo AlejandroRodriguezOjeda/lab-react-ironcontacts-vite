d41045fb1a74faa0a03822af0f7dca716ab927fa
Alejandro Rodriguez <aperturegir@gmail.com>
2023-08-23 07:53:09 +0100

    fixed console warnings

--- src/App.jsx
+++ src/App.jsx
@@ -3,91 +3,97 @@ import allContacts from "./contacts.json";
 import { useState } from "react";
 
 function App() {
- const [contacts, setContacts] = useState(allContacts.slice(0,5))
+  const [contacts, setContacts] = useState(allContacts.slice(0, 5));
 
- const handleAddrandomContact = () =>{
-  console.log("intentando añadir un contacto");
-  let randomNumber = Math.random() * allContacts.length
-  let randomIndex = Math.floor(randomNumber)
-  let randomContact = allContacts[randomIndex]
+  const handleAddrandomContact = () => {
+    console.log("intentando añadir un contacto");
+    let randomNumber = Math.random() * allContacts.length;
+    let randomIndex = Math.floor(randomNumber);
+    let randomContact = allContacts[randomIndex];
 
-  let repeatedContact = contacts.find((eachContact)=>{
-    if(eachContact.id === randomContact.id){
-      return true
+    let repeatedContact = contacts.find((eachContact) => {
+      if (eachContact.id === randomContact.id) {
+        return true;
+      }
+    });
+
+    if (repeatedContact !== undefined) {
+      handleAddrandomContact();
+      return;
     }
-  })
 
-  if(repeatedContact !== undefined) {
-    handleAddrandomContact()
-    return;
-  }
- 
+    let clone = JSON.parse(JSON.stringify(contacts));
+    clone.unshift(randomContact);
 
- let clone = JSON.parse(JSON.stringify(contacts))
- clone.unshift(randomContact)
+    setContacts(clone);
+  };
+  const handleSortContactsByName = () => {
+    console.log("intentando ordenar");
+    let clone = JSON.parse(JSON.stringify(contacts));
+    clone.sort((con1, con2) => {
+      return con1.name > con2.name ? 1 : -1;
+    });
+    setContacts(clone);
+  };
 
- setContacts(clone)
-}
-const handleSortContactsByName = () => {
-  console.log("intentando ordenar");
-  let clone = JSON.parse(JSON.stringify(contacts))
-  clone.sort((con1, con2) =>{
-    return con1.name > con2.name ? 1 : -1
-  })
-  setContacts(clone)
-}
+  const handleSortContactsByPopularity = () => {
+    console.log("intentando ordenar");
+    let clone = JSON.parse(JSON.stringify(contacts));
+    clone.sort((con1, con2) => {
+      return con1.popularity > con2.popularity ? -1 : 1;
+    });
+    setContacts(clone);
+  };
 
-const handleSortContactsByPopularity = () => {
-  console.log("intentando ordenar");
-  let clone = JSON.parse(JSON.stringify(contacts))
-  clone.sort((con1, con2) =>{
-    return con1.popularity > con2.popularity ? -1 : 1
-  })
-  setContacts(clone)
-}
-
-const handleDeleteContact = (id) => {
-  console.log("intentando borrar");
-let filterArr = contacts.filter((eachContact)=> eachContact.id !== id)
-setContacts(filterArr)
-}
+  const handleDeleteContact = (id) => {
+    console.log("intentando borrar");
+    let filterArr = contacts.filter((eachContact) => eachContact.id !== id);
+    setContacts(filterArr);
+  };
 
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
 
-  <button onClick={handleAddrandomContact}>Add Random Contact</button>
-  <button onClick={handleSortContactsByName}>Sort by Name</button>
-  <button onClick={handleSortContactsByPopularity}>Sort by Popularity</button>
+      <button onClick={handleAddrandomContact}>Add Random Contact</button>
+      <button onClick={handleSortContactsByName}>Sort by Name</button>
+      <button onClick={handleSortContactsByPopularity}>
+        Sort by Popularity
+      </button>
 
-        <table>
-          <thead>
-  <tr>
-    <th>Picture</th>
-    <th>Name</th>
-    <th>Popularity</th>
-    <th>Won Oscar</th>
-    <th>Won Emmy</th>
-    </tr>
-  </thead>
-  <tbody>
-
-  {contacts.map((eachContact) =>(
-        
-  <tr>
-    <td><img src={eachContact.pictureUrl} alt={eachContact.name} style={{width: '200px'}} /></td>
-    <td>{eachContact.name}</td>
-    <td>{eachContact.popularity}</td>
-    <td>{eachContact.wonOscar ? "🏆" : null}</td>
-    <td>{eachContact.wonEmmy ? "🌟" : null}</td>
-    <button onClick={() => handleDeleteContact(eachContact.id)}>Delete</button>
-  </tr>
- 
-
-        
-      ))}
-      </tbody> 
-</table>
+      <table>
+        <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won Oscar</th>
+          <th>Won Emmy</th>
+        </tr>
+</thead>
+<tbody>
+        {contacts.map((eachContact, i) => (
+          <tr key={i}>
+            <td>
+              <img
+                src={eachContact.pictureUrl}
+                alt={eachContact.name}
+                style={{ width: "200px" }}
+              />
+            </td>
+            
+            <td>{eachContact.name}</td>
+            <td>{eachContact.popularity.toFixed(2)}</td>
+            <td>{eachContact.wonOscar ? "🏆" : null}</td>
+            <td>{eachContact.wonEmmy ? "🌟" : null}</td>
+         <td><button onClick={() => handleDeleteContact(eachContact.id)}>
+              Delete
+            </button></td> 
+          </tr>
+          
+        ))}
+        </tbody>
+      </table>
     </div>
   );
 }
